04385b293a4596d80341ef098e956c92bd2b62a2
bsjinn <baekso94@nate.com>
2017-06-28 20:36:11 +0900

    start_setup

--- app/src/main/java/com/example/sojin/busbellapp/activity/MainActivity.java
+++ app/src/main/java/com/example/sojin/busbellapp/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.sojin.busbellapp;
+package com.example.sojin.busbellapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.sojin.busbellapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/sojin/busbellapp/application/BusBellApp.java
@@ -0,0 +1,16 @@
+package com.example.sojin.busbellapp.application;
+
+import android.app.Application;
+
+/**
+ * Created by sojin on 2017-06-28.
+ */
+
+public class BusBellApp extends Application {
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+    }
+
+}
